07a920a2a06a30262a9ac3f004889b0d083873f2
Rinaberger <scottrinaberger@gmail.com>
2022-08-12 15:08:46 -0500

    set up loop for status colors; timeValue is undefined

--- Develop/script.js
+++ Develop/script.js
@@ -8,18 +8,40 @@ $(document).ready(function() {
 
 
 
-
 //functions
-function saveText(event) {
-    let notesValue = $(this).siblings('.hour_notes').val()
+function saveText(event) {  
     let timeValue = $(this).parent().attr('data-time');
+    let notesValue = $(this).siblings('.hour_notes').val()
     console.log(notesValue);
     console.log(timeValue);
     //save text area to localStorage
     localStorage.setItem(timeValue, notesValue);           
     }
 
-
+// change box alerts by time    
+function setHourAlert() {
+    //establish current time
+    let hourNow = moment().hours();
+    console.log(hourNow);
+    //bring in schedule time
+    let timeValue = $(this).parent().attr('data-time');
+    console.log(timeValue);
+    
+    //determine status
+    $('.hour_box').each(function() {
+        //let entryHour = $(this).parent().attr('data-time');
+        if (timeValue < hourNow) {
+            $(this).addClass('future');
+            console.log('future')
+        } else if (timeValue === hourNow) {
+            $(this).addClass('present');
+            console.log('present')
+        } else { 
+            $(this).addClass('past');
+            console.log('past')
+        }    
+    })
+}
     
 //events
 
