27bbf6f0c8c57a52c378eb098eb459de030a43e3
Scott Rinaberger <scottrinaberger@gmail.com>
2022-08-15 21:48:16 -0500

    implemented status colors

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,22 @@
 // all functions run as part of whole document
 $(document).ready(function() {
 
-//variables
+    let timeValue = $('#9').attr('data-time');
+    console.log(timeValue + " = hardcoded") 
+    console.log(timeValue) //log time value
 
 
-
+//issue 1: timeValue === doesn't work
+//issue 2: red won't appear; stays grey so not understanding current hour. Tricked it 
 
 
 
 //functions
 function saveText(event) {  
-    let timeValue = $(this).parent().attr('data-time');
+    timeValue = $(this).parent().attr('data-time');
     let notesValue = $(this).siblings('.hour_notes').val()
-    console.log(notesValue);
-    console.log(timeValue);
+    console.log(notesValue + " = notes");
+    console.log(timeValue + " = hour notes inputted");
     //save text area to localStorage
     localStorage.setItem(timeValue, notesValue);           
     }
@@ -22,23 +25,23 @@ function saveText(event) {
 function setHourAlert() {
     //establish current time
     let hourNow = moment().hours();
-    console.log(hourNow);
+    console.log(hourNow + " = current hour");
     //bring in schedule time
-    let timeValue = $(this).parent().attr('data-time');
-    console.log(timeValue);
+    //let timeValue = timeValueAll;
+    console.log(timeValue + " = schedule hour");
     
     //determine status
     $('.hour_box').each(function() {
         //let entryHour = $(this).parent().attr('data-time');
-        if (timeValue < hourNow) {
+        if (timeValue > hourNow) {
             $(this).addClass('future');
             console.log('future')
-        } else if (timeValue === hourNow) {
-            $(this).addClass('present');
-            console.log('present')
-        } else { 
+        } else if (timeValue < hourNow) {
             $(this).addClass('past');
             console.log('past')
+        } else { 
+            $(this).addClass('present');
+            console.log('present')
         }    
     })
 }
